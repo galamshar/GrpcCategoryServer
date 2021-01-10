2a11772571cb494bb9c42fecf1418cc573b09b3b
galamshar <myrzagalyev.galymzhan@gmail.com>
2021-01-10 23:25:52 +0600

    Add project files.

---
+++ GrpcSE1Server/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using GrpcCategoryServer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcCategoryServer.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> CategoryList { get; set; }
+        public DbSet<Product> ProductList { get; set; }
+    }
+}
